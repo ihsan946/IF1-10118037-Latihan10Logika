d26f8176f7db9918ae7ea5443f1a3db07bb3c6e9
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 14:56:15 +0700

    Add files via upload

---
+++ IF110118037Latihan10Logika/src/if110118037latihan10logika/IF110118037Latihan10Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan10logika;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program Logika.
+ */
+public class IF110118037Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
